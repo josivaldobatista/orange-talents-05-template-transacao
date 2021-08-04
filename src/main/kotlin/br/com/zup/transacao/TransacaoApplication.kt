76417db26a200a44bed002904cb4d89fce784170
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-08-04 14:17:19 -0300

    Project created

---
+++ src/main/kotlin/br/com/zup/transacao/TransacaoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.transacao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransacaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransacaoApplication>(*args)
+}
